50b022e3b091acdf8f4ea9ed3bfbb7cfd77fc61e
Elfen <17817602347@163.com>
2025-12-25 09:09:51 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,39 @@
+const siteData = {
+    projects: [
+        {
+            title: "🎄 Golden 3D Star Christmas Tree",
+            date: "2025-12-25",
+            desc: "A mesmerizing 3D Christmas tree animation with rotating stars and snow effects.",
+            link: "code/christmas_tree_20251225.html"
+        },
+        {
+            title: "❤️ Heart Animation",
+            date: "2025-12-22",
+            desc: "An interactive heart popup animation with confetti effects and floating particles.",
+            link: "code/heart_animation_20251222.html"
+        }
+    ],
+    photos: {
+        "2026": {
+            "January": [
+                { src: "photo/2026/01/1.jpg", caption: "Jan 2026 - Moment 1" },
+                { src: "photo/2026/01/2.jpg", caption: "Jan 2026 - Moment 2" }
+            ]
+        },
+        "2025": {
+            "February": [
+                { src: "photo/2025/02/1.jpg", caption: "Feb 2025 - Moment 1" },
+                { src: "photo/2025/02/2.png", caption: "Feb 2025 - Moment 2" }
+            ],
+            "January": [
+                { src: "photo/2025/01/1.jpg", caption: "Jan 2025 - Moment 1" }
+            ]
+        }
+    },
+    music: [
+        { title: "七里香", src: "bgm/周杰伦 - 七里香.mp3" },
+        { title: "甜甜的", src: "bgm/周杰伦 - 甜甜的.mp3" },
+        { title: "简单爱", src: "bgm/周杰伦 - 简单爱.mp3" }
+    ],
+    anniversary: "2025-12-13" // 示例日期：2024年5月20日，請修改為你們的紀念日
+};
